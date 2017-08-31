874ea9d7a810f219d16e3b6e249dacdcd1102eeb
ngbricen <bricen@gmail.com>
2017-08-31 08:17:21 -0400

    Adjusting for heroku deployment

--- models/index.js
+++ models/index.js
@@ -4,15 +4,19 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'production';
+var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
+console.log('process.env.NODE_ENV (in index.js) = ' + process.env.NODE_ENV);
+
+var sequelize;
 if (config.use_env_variable) {
-  console.log("HERE " + config.use_env_variable);
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  console.log('HERE 1, config.use_env_variable = ' + config.use_env_variable);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  console.log('HERE 2, config.use_env_variable = ' + config.use_env_variable);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
