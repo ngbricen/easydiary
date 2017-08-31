9f176db56b87144d1ac2d41f99b0c1801decf69f
ngbricen <bricen@gmail.com>
2017-08-31 09:48:01 -0400

    Adjustment for heroku deployment

--- models/index.js
+++ models/index.js
@@ -4,21 +4,29 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
+var env       = process.env.NODE_ENV || 'production';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 console.log('process.env.NODE_ENV (in index.js) = ' + process.env.NODE_ENV);
 
 var sequelize;
-if (config.use_env_variable) {
-  console.log('HERE 1, config.use_env_variable = ' + config.use_env_variable);
-  console.log('HERE 1, process.env = ' + process.env[config.use_env_variable]);
-  sequelize = new Sequelize(process.env.JAWSDB_URL, {"dialect": "mysql"});
-} else {
-  console.log('HERE 2, config.use_env_variable = ' + config.use_env_variable);
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (process.env.JAWSDB_URL){ 
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
 }
+else { 
+  var sequelize = new Sequelize("tyqyc0s23aq7w7io", "nu3tggxsnmknn9f3", "zwoyjvf8xo1k182g", {
+    port: 3306,
+    host: "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    dialect: "mysql",
+    pool: {
+      max: 10,
+      min: 0,
+      idle: 30000
+    }
+  });
+}
+
 
 fs
   .readdirSync(__dirname)
